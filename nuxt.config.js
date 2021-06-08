a1f090cc91d269c1984bc9dd006f096bfb9586a6
Farid <fnikman@gmail.com>
2021-06-08 14:55:14 +0800

    feat: update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'weather-app',
+    titleTemplate: 'Weather Forecast',
     title: 'Weather Forecast',
     htmlAttrs: {
       lang: 'en',
