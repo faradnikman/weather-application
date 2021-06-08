5efecd59e38b12895d542bc1a68a7c5f5042c807
Farid <fnikman@gmail.com>
2021-06-08 09:44:17 +0800

    fix: remove card in weatherCard

--- components/WeatherCard/index.vue
+++ components/WeatherCard/index.vue
@@ -1,29 +1,30 @@
 <template>
-  <v-wait for="fetch-daily-data">
-    <template slot="waiting">
-      <v-skeleton-loader type="card"></v-skeleton-loader>
-    </template>
-    <v-card v-if="daily.name">
-      <v-card-title
-        >{{ daily.name }}
-        <v-btn
-          icon
-          @click.prevent="addCities(daily.name)"
-          v-if="isNotIncludedInCities"
-        >
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </v-card-title>
-      <v-card-text>
-        <h2>{{ currentTemperature.temp }}</h2>
-        <h3>
-          Feels like {{ currentTemperature.feels_like }},
-          {{ currentWeather.description }}
-        </h3>
-      </v-card-text>
-      <div></div>
-    </v-card>
-  </v-wait>
+  <v-flex class="mt-10">
+    <v-wait for="fetch-daily-data">
+      <template slot="waiting">
+        <v-skeleton-loader type="card"></v-skeleton-loader>
+      </template>
+      <v-flex v-if="daily.name">
+        <v-flex
+          >{{ daily.name }}
+          <v-btn
+            icon
+            @click.prevent="addCities(daily.name)"
+            v-if="isNotIncludedInCities"
+          >
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex>
+          <h2>{{ currentTemperature.temp }}</h2>
+          <h3>
+            Feels like {{ currentTemperature.feels_like }},
+            {{ currentWeather.description }}
+          </h3>
+        </v-flex>
+      </v-flex>
+    </v-wait></v-flex
+  >
 </template>
 
 <script>
