d76b59ab3d21462dfb403a47fc48f1c8f3228e0e
Farid <fnikman@gmail.com>
2021-06-08 09:43:06 +0800

    feat: implement add carousel layout

---
+++ components/CitiesCarousel/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-flex>
+    <h2>Weather Forecast</h2>
+    <v-wait for="fetch-cities-daily">
+      <template slot="waiting">
+        <v-row class="ma-n2">
+          <v-col
+            xs="12"
+            sm="6"
+            md="4"
+            v-cloak
+            v-for="n in 3"
+            :key="n"
+            class="pa-2"
+          >
+            <v-skeleton-loader type="card" />
+          </v-col>
+        </v-row>
+      </template>
+      <v-row class="ma-n2">
+        <v-col
+          md="3"
+          xs="12"
+          v-cloak
+          v-if="cities.length"
+          v-for="(city, index) in cities"
+          :key="city"
+          class="pa-2"
+        >
+          <v-card height="100%">
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col>
+                    <h2>{{ city }}</h2>
+                    <v-btn icon @click.prevent.stop="removeCities(index)">
+                      <v-icon>mdi-minus</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-wait>
+  </v-flex>
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex'
+
+export default {
+  name: 'CitiesCarousel',
+  computed: {
+    ...mapState('weather', ['cities']),
+  },
+  methods: {
+    ...mapMutations('weather', ['removeCities']),
+  },
+}
+</script>
